254df30631d7ac0e945f93f6573756b21b462a01
pimentelloyz <andre.loyz@gmail.com>
2017-02-14 14:47:30 -0200

    Initial Commit

---
+++ SabbathSchool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SabbathSchool
+//
+//  Created by André Pimentel on 14/02/17.
+//  Copyright © 2017 IASD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
