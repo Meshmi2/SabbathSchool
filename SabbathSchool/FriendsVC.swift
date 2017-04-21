850309ee8b13242ade48c061944c5974dfaa6352
Emerson Carpes <emerson.carpes@gmail.com>
2017-04-21 14:58:37 -0300

    sabbath school students screen

--- SabbathSchool/FriendsVC.swift
+++ SabbathSchool/FriendsVC.swift
@@ -12,11 +12,15 @@ import ObjectMapper
 import Alamofire
 import AlamofireObjectMapper
 
-class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FriendsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     var user = [User]()
     
-    var friend: [Friend_]?
+    var students = [[Friend_]]()
+    
+    //var sectionId:String!
     
     var initialInformationCard: [cFriends]?
     
@@ -39,11 +43,24 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
         loadUser()
        
-        getFriend()
+        //getFriend()
         
         getFriendCoreData()
+        
+        setupNavigationBar()
    
     }
+    
+    // MARK: Setup
+    
+    fileprivate func setupNavigationBar(){
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 38.0, height: 30.0))
+        imageView.image = UIImage(named: "logo_cabecalho")
+        imageView.contentMode = .scaleAspectFit
+        navigationItem.titleView = imageView
+    }
+    
+    // MARK: DataSource
 
     
     func loadUser() {
@@ -70,7 +87,7 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func getFriend() {
         
-        let getInfoParameters: [String : Any] = ["operacao": "getAmigos", "classeId": self.classId, "data": self.functionId]
+        let getInfoParameters: [String : Any] = ["operacao": "getAmigos", "classeId": 7793/*self.classId*/, "data": 7/*self.functionId*/]
         
         let getInfoEndpoint: String = "http://test-sistemas.usb.org.br/escolasabatina/APIMobile/metodos/amigosEstudo/index_controller.php"
         
@@ -93,6 +110,8 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 if let someFriends = data.friends {
                     for _friends in someFriends {
                         
+                        print(_friends.name)
+                        
                         let appDel: AppDelegate = (UIApplication.shared.delegate as! AppDelegate)
                         self.contextObject = appDel.managedObjectContext
                         self.newFriend = NSEntityDescription.insertNewObject(forEntityName: "Friend_", into: self.contextObject)
@@ -103,12 +122,13 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                         if let instructor = _friends.instructor {
                             self.newFriend.setValue(instructor, forKey: "instructor_")
                             if _friends.instructor == true {
-                                self.newInstructor.setValue(instructor, forKey: "name_")
+                                self.newInstructor.setValue(_friends.nameInstructor, forKey: "name_")
                             }
                         }
                         if let name = _friends.name {
                             self.newFriend.setValue(name, forKey: "name_")
                         }
+                        
                         if let nameInstructor = _friends.nameInstructor {
                             self.newFriend.setValue(nameInstructor, forKey: "nameInstructor_")
                         }
@@ -126,6 +146,7 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                         }
                     }
                 }
+                self.tableView.reloadData()
             }
         }
     }
@@ -136,7 +157,20 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let presentRequest:NSFetchRequest<Friend_> = Friend_.fetchRequest()
         
         do {
-            friend = try managedObjectContext.fetch(presentRequest)
+            
+            let friend = try managedObjectContext.fetch(presentRequest).filter({$0.instructor_ == true})
+            for i in friend {
+                var studentsTemp = [Friend_]()
+                var friend:[Friend_]
+                studentsTemp.append(i)
+                friend = try managedObjectContext.fetch(presentRequest).filter({$0.nameInstructor_ == i.name_})
+                for e in friend {
+                    studentsTemp.append(e)
+                }
+                self.students.append(studentsTemp)
+            }
+            //self.students.append(students1)
+            self.tableView.reloadData()
         } catch {
             appDelegate.errorView("Isso é constrangedor! \(error.localizedDescription)")
         }
@@ -165,35 +199,41 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
         
     }
-
- 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if self.friend == nil {
-            return 0
-        }
-        return (self.friend?.count)!
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.friendsCell, for: indexPath) as! FriendTVCell
-        
-        cell.selectionStyle = UITableViewCellSelectionStyle.none
-        
-        let initialInformationFriend = friend?[indexPath.row]
-        
-        cell.instructorLabel.text = initialInformationFriend?.nameInstructor_
-        
-        return cell
-        
-    }
   
     @IBAction func closeButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension FriendsVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableCell(withIdentifier: "CellFriend") as! FriendTVCell
+        header.instructorLabel.text = self.students[section][0].name_
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 55.0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.students.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.students[section].count - 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellStudent", for: indexPath) as! StudentTVCell
+        cell.selectionStyle = .none
+        cell.nameLabel.text = self.students[indexPath.section][indexPath.row + 1].name_
+        return cell
+    }
 }
